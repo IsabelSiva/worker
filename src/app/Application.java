63c7978808c7f85effd7d5652ef73a2de3284212
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2022-07-08 14:36:19 -0300

    chore:modify Worker to receive a list of contracts, make some getters and setters in to the classes

---
+++ src/app/Application.java
@@ -0,0 +1,10 @@
+package app;
+
+import entities.Worker;
+
+public class Application {
+    public static void main(String[]bla){
+        Worker worker;
+
+    }
+}
--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,22 +1,82 @@
 package entities;
 
+import entities.enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class Worker {
     private String name;
     private WorkerLevel level;
     private double baseSalary;
 
-    public Worker(String name, WorkerLevel workerLevel, double baseSalary){
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker(){}
+    public Worker(String name, WorkerLevel workerLevel, double baseSalary, Department department){
         this.name = name;
         this.level = workerLevel;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
-    public void addContract(HourContract contract){
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
     }
     public void removeContract(HourContract contract){
-
+        contracts.remove(contract);
     }
     public double income(int year, int month){
-        return baseSalary;
+        double income = 0;
+        Calendar calendar = Calendar.getInstance();
+
+        for(HourContract c : contracts){
+            calendar.setTime(c.getDate());
+            int contractYear = calendar.get(Calendar.YEAR);
+            int contractMonth = calendar.get(Calendar.MONTH);
+
+            if (contractYear == year && contractMonth == month){
+                income = baseSalary + c.totalValue();
+            }
+        }
+        return income;
     }
 }
